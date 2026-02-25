677d14cde4e1a6e05a16f576d12317cd7aa51241
Arjun Chatha <arjunchatha13@gmail.com>
2026-02-24 17:40:47 -0700

    Rename supply.java to Supply.java

--- Supply.java
+++ Supply.java
@@ -6,10 +6,13 @@ public class Supply {
 
     public Supply(String type, int quantity) {
         this.type = type;
+       
         this.quantity = quantity;
     }
 
     public String getType() { return type; }
+    
+    
     public void setType(String type) { this.type = type; }
     public int getQuantity() { return quantity; }
 
