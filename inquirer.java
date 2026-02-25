8020916561860f60c6eb6cf6650d15f32a179887
Arjun Chatha <arjunchatha13@gmail.com>
2026-02-24 17:34:07 -0700

    inquirer class done

---
+++ inquirer.java
@@ -0,0 +1,21 @@
+package edu.ucalgary.oop;
+
+public class Inquirer {
+    private String firstName;
+    private String lastName;
+    private String servicesPhoneNum;
+    private String info;
+
+    public Inquirer(String firstName, String lastName, String phone, String info) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.servicesPhoneNum = phone;
+        this.info = info;
+    }
+
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getServicesPhoneNum() { return servicesPhoneNum; }
+    public String getInfo() { return info; }
+}
+
